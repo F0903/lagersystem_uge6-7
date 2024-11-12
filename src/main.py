a59b2219c0b3e32c2ea7e282f45a50672f9d40ec
isa <61465458+isaDue99@users.noreply.github.com>
2024-11-12 10:41:41 +0100

    Restored main.py

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ from db.db import Db
 from product_manager import ProductManager
 
 if __name__ == "__main__":
-    #db = Db(user="root", password="root", host="localhost", database="lager")
+    db = Db(user="root", password="root", host="localhost", database="lager")
 
-    pm = ProductManager()
-    my_shirt = pm.new_product("Clothing")
-    my_shirt.Name = "my cool new shirt"
-    my_shirt.Color = "red"
-    print(my_shirt.get_sql_values())
-    my_shirt.Description = "it is red"
-    print(my_shirt.get_sql_values())
-    print(vars(my_shirt))
+    # pm = ProductManager()
+    # my_shirt = pm.new_product("Clothing")
+    # my_shirt.Name = "my cool new shirt"
+    # my_shirt.Color = "red"
+    # print(my_shirt.get_sql_values())
+    # my_shirt.Description = "it is red"
+    # print(my_shirt.get_sql_values())
+    # print(vars(my_shirt))
