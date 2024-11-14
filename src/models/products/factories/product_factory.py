d73e1598bfb5549ad72a1c8fb8edaa0bddb7e12a
Frederik P. <frepbh@gmail.com>
2024-11-14 13:30:54 +0100

    added a create product_from_dict method + added more documentation

--- src/models/products/factories/product_factory.py
+++ src/models/products/factories/product_factory.py
@@ -1,25 +1,60 @@
+import dataclasses
+from typing import Any
+
 from ... import products
 
 
-def create_product(
-    type: str, descriptor: products.ProductDescriptor, **extra_attributes
+# TODO move to product as class methods?
+
+
+def _get_product_class_dynamically(type: str):
+    """
+    Dynamically get the class type by name from the products module.
+    So for example, if the "type" string is "Clothing", it will return
+    the Clothing type.
+    """
+
+    product_class = getattr(products, type)
+    return product_class
+
+
+def create_product_from_descriptor(
+    descriptor: products.ProductDescriptor, **extra_attributes
 ) -> products.Product:
     """
     Dynamically get the product class from the "products" module by class name.
+
+    'extra_attributes' represents the additional arguments provided to the constructor of the class.
 
     Returns:
         The new product instance.
     """
 
     try:
-        # Dynamically get the class type by name from the products module.
-        # So for example, if the "type" string is "Clothing", it will return
-        # the Clothing type.
-        product_class = getattr(products, type)
-
-        # Create an instance of the type we just got.
+        type = descriptor.Type
+        product_class = _get_product_class_dynamically(type)
         product_instance = product_class(Descriptor=descriptor, **extra_attributes)
 
         return product_instance
     except AttributeError:
-        raise ValueError(f"Unknown product type: {type}")
+        raise ValueError(f"Unknown product type")
+
+
+def create_product_from_dict(dict: dict[str, Any]) -> products.Product:
+    """
+    Create the product dynamically fron a dictionary of fields.
+
+    Requires that it matches an existing product type.
+    """
+
+    descriptor_dict = dict["Descriptor"]
+    descriptor = products.ProductDescriptor.create_from_dict(descriptor_dict)
+
+    # TODO: perhaps validate the keys in the dict?
+
+    # Get all other props in the dict that isn't "Descriptor"
+    other_props = {key: value for key, value in dict.items() if key != "Descriptor"}
+
+    product = create_product_from_descriptor(descriptor, **other_props)
+
+    return product
