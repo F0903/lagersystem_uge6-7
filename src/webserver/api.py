71216702db0890e944aea2f6fa0db64aa8249ab6
Frederik P. <frepbh@gmail.com>
2024-11-14 15:09:07 +0100

    api can now set and add products

--- src/webserver/api.py
+++ src/webserver/api.py
@@ -1,23 +1,18 @@
 from typing import Any
-from flask import Flask, jsonify, request
-
+from flask import Flask, Request, jsonify, request, Response
 from db.adapters.product_adapter import ProductAdapter
 from db.db_connection import DbConnection
 from models.products import Product, ProductDescriptor, create_product_from_dict
+import db.db_error as db_err
 
 api = Flask(__name__)
 db = DbConnection("lager")
 product_adapter = ProductAdapter(db)
 
 
-@api.route("/api/get/products")
-def get_products() -> list[Product]:
-    product_iter = product_adapter.get_all_products(None)
-    # We need to return a list and not an iterator.
-    return list(product_iter)
-
-
-def _validate_product_fields(product_data: dict[str, Any]):
+def _validate_product_fields(
+    product_data: dict[str, Any]
+) -> tuple[Response, int] | None:
     """
     Validate that the request data has the minimum required fields.
     """
@@ -34,13 +29,7 @@ def _validate_product_fields(product_data: dict[str, Any]):
         )
 
 
-# We don't use a parameter, as we get the ID directly off of the body
-@api.route("/api/set/product", methods=["PUT"])
-def set_product():
-    """
-    Replace a particular product by ID with the body of this request.
-    """
-
+def _validate_product_request(request: Request) -> tuple[Response, int] | dict:
     # Ensure the request body is JSON
     if not request.is_json:
         return jsonify({"error": "Request body must be JSON"}), 400
@@ -50,5 +39,48 @@ def set_product():
     if validate_error:
         return validate_error
 
-    product = create_product_from_dict(product_data)
-    product_adapter.update_product(product)
+    return product_data
+
+
+@api.route("/api/get/products")
+def get_products() -> list[Product]:
+    product_iter = product_adapter.get_all_products(None)
+    # We need to return a list and not an iterator.
+    return list(product_iter)
+
+
+@api.route("/api/add/product", methods=["POST"])
+def add_product():
+    """
+    Insert a product defined by the body of this request.
+    """
+
+    assert_result = _validate_product_request(request)
+    if isinstance(assert_result, tuple):  # Is result an error response tuple?
+        return assert_result
+
+    product = create_product_from_dict(assert_result)
+
+    try:
+        product_adapter.insert_product(product)
+    except db_err.DbError as err:
+        return jsonify({"error": err.message}, 400)
+
+
+# We don't use a parameter, as we get the ID directly off of the body
+@api.route("/api/set/product", methods=["PUT"])
+def set_product():
+    """
+    Replace a particular product by ID with the body of this request.
+    """
+
+    assert_result = _validate_product_request(request)
+    if isinstance(assert_result, tuple):  # Is result an error response tuple?
+        return assert_result
+
+    product = create_product_from_dict(assert_result)
+
+    try:
+        product_adapter.update_product(product)
+    except db_err.DbError as err:
+        return jsonify({"error": err.message}, 400)
