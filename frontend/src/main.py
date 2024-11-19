ad9a58d1a7da89a8128fa5778283216e99fa5240
Frederik P. <frepbh@gmail.com>
2024-11-19 10:45:28 +0100

    removed the .venv as it was impossible to get working

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,5 +1,23 @@
 import click
+import requests
+
+HOST = "localhost"
+PORT = 5000
+URL_BASE = f"http://{HOST}:{PORT}"
+
 
 @click.command()
-def get():
+@click.option("--filter", help="Filter for product type.")
+def get_product(filter: str | None):
+    response = requests.get(URL_BASE + "/api/products")
+    body = response.json()
+    print(body)
+
+
+@click.command()
+def set_product():
     pass
+
+
+if __name__ == "__main__":
+    get_product()
